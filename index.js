3078c7cdbb9d87c6196ac1f8ee8c7727e1e3bcdd
Atul Awasthi <atul.awasthi@theasianparent.com>
2022-08-19 00:21:50 +0530

    rds: fix progress-bar colorization

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function func() {
-  const x = document.getElementById("myProgress");
+    const x = document.getElementById("myProgress");
+    x.value = document.getElementById("value").value;
 
-  x.value = document.getElementById("value").value;
+    if (x.value < 20) {
+        x.classList.remove("progress-green");
+        x.classList.add("progress-red");
+    } else if (x.value >= 20 && x.value <= 65) {
+        x.classList.remove("progress-green", "progress-red");
+        x.classList.add("progress-yellow");
+    } else {
+        x.classList.remove("progress-yellow", "progress-red");
+        x.classList.add("progress-green");
+    }
 }
