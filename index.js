57f111e490f793c891d4a21d09235b96928eb68f
Atul <atulreso1@gmail.com>
2022-08-19 09:03:17 +0530

    rds: handle submit

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 function barFn() {
-    const inputRef = document.getElementById("myProgress");
-    inputRef.value = document.getElementById("value").value;
+  const inputRef = document.getElementById("myProgress");
+  inputRef.value = document.getElementById("value").value;
 
-    if (inputRef.value < 20) {
-        inputRef.classList.remove("progress-green");
-        inputRef.classList.add("progress-red");
-    } else if (inputRef.value >= 20 && inputRef.value <= 65) {
-        inputRef.classList.remove("progress-green", "progress-red");
-        inputRef.classList.add("progress-yellow");
-    } else {
-        inputRef.classList.remove("progress-yellow", "progress-red");
-        inputRef.classList.add("progress-green");
-    }
+  if (inputRef.value < 20) {
+    inputRef.classList.remove("progress-green");
+    inputRef.classList.add("progress-red");
+  } else if (inputRef.value >= 20 && inputRef.value <= 65) {
+    inputRef.classList.remove("progress-green", "progress-red");
+    inputRef.classList.add("progress-yellow");
+  } else {
+    inputRef.classList.remove("progress-yellow", "progress-red");
+    inputRef.classList.add("progress-green");
+  }
 }
+
+/*
+const submitBtn = () => {
+  barFn();
+  return barFn();
+  return {
+    ...barFn(),
+    hay: "success",
+  };
+};
+*/
